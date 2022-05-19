b2687abd91046207fd9d0de078a0b35e1c6792ff
jarviscodes <31391792+jarviscodes@users.noreply.github.com>
2022-05-19 13:55:17 +0200

    Added Transpose Cipher Encryption

--- transpose_cipher/__main__.py
+++ transpose_cipher/__main__.py
@@ -1,3 +1,5 @@
 from transpose_cipher.translator import TransposeTranslator
 
-translation = TransposeTranslator.encrypt_message("Hallo Test 123", 4)
+translation = TransposeTranslator.encrypt_message("Hallo Test 123", 5)
+
+print(translation)
--- transpose_cipher/translator.py
+++ transpose_cipher/translator.py
@@ -1,16 +1,21 @@
-import textwrap
-
 class TransposeTranslator(object):
     @classmethod
-    def encrypt_message(cls, text, key):
+    def encrypt_message(cls, text, key, padding="#"):
         if key > len(text) / 2:
             print(f"Key max length for this text is {int(len(text) / 2)}")
             exit()
         else:
             all_chunks = [text[i:i+key] for i in range(0, len(text), key)]
-            for chunk in all_chunks:
-                chunk += '#' * (key - len(chunk))
+            for i in range(len(all_chunks)):
+                all_chunks[i] += f'{padding}' * (key - len(all_chunks[i]))
 
+            # We should now have n padded chunks of len = key each.
+            full_cipher_padded = ""
+            for j in range(key):
+                for chunk in all_chunks:
+                    full_cipher_padded += chunk[j]
+
+            return full_cipher_padded.replace("#", "")
 
 
 
